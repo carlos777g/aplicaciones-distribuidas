e08f0e9d7121759650621da39dea589dd0f1ef34
Carlos Guillen <gcarlosjael@gmail.com>
2025-06-26 21:26:11 -0600

    Agregando la práctica de mongo que me faltaba - Se solucionó el error de autenticación cuando el usuario En realidad sólo agregue la parte de mongo que sí había hecho pero que no guardé porque fui modificando ese archivo con el paso del tiempo, pero sólo era una conexión básica a mongo y se pedían el nombre de las bases de datos guardadas

---
+++ mongo-1/index.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main() {
+const uri = "mongodb+srv://car:1jShLimcHnoEivV4@cluster0.0du5kp8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    await listDatabases(client);
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
